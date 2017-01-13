5022e024c50962a49fb62a2e33ce779ffff6fe0d
Denny de la Haye <denny.d@38degrees.org.uk>
2017-01-13 00:17:08 +0000

    Add settings.yml and some partials etc, to support visual themes.

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,12 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module S3filemanager
+module MahBucket
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.settings = config_for( :settings )
   end
 end
