d56e957431a07c5ffda8cdbe15107844f4bce4f4
Denny de la Haye <denny.d@38degrees.org.uk>
2017-01-31 23:07:36 +0000

    Unhardcode config items

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -5,7 +5,7 @@ if ENV['AWS_ACCESS_KEY_ID']
   Paperclip::Attachment.default_options[ :storage        ] = :s3
   Paperclip::Attachment.default_options[ :s3_protocol    ] = 'https'
   Paperclip::Attachment.default_options[ :url            ] = ':s3_alias_url'
-  Paperclip::Attachment.default_options[ :s3_host_alias  ] = 'files.38degrees.org.uk'
+  Paperclip::Attachment.default_options[ :s3_host_alias  ] = ENV.fetch( 'S3_HOST_ALIAS' )
   Paperclip::Attachment.default_options[ :path           ] = '/:class/:attachment/:id_partition/:style/:filename'
   Paperclip::Attachment.default_options[ :s3_headers     ] = { 'Cache-Control' => 'max-age=315576000', 'Expires' => 10.years.from_now.httpdate }
   Paperclip::Attachment.default_options[ :s3_permissions ] = 'public-read'
@@ -13,6 +13,6 @@ if ENV['AWS_ACCESS_KEY_ID']
   Paperclip::Attachment.default_options[ :s3_credentials ] = {
       access_key_id:     ENV.fetch( 'AWS_ACCESS_KEY_ID'     ),
       secret_access_key: ENV.fetch( 'AWS_SECRET_ACCESS_KEY' ),
-      bucket:            ENV.fetch( 'S3_BUCKET'             ) { 'public38' },
+      bucket:            ENV.fetch( 'S3_BUCKET'             ),
   }
 end
