4d9c885d7a2f5b2829387eb6f3ecdb4fbe97da83
Denny de la Haye <denny.d@38degrees.org.uk>
2017-01-31 22:29:37 +0000

    More work on tag features

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -6,9 +6,14 @@ class TagsController < ApplicationController
     @tags = ActsAsTaggableOn::Tag.all.order( :name )
   end
 
+  def tag_cloud
+    @tags = Item.tag_counts_on( :customs )
+  end
+
   # GET /tags/1
   # GET /tags/1.json
   def show
+    @tag = ActsAsTaggableOn::Tag.find( params[:id] )
   end
 
 end
