58bac5a0293aea7d65900eba9e5dca0c51454dfb
Denny de la Haye <denny.d@38degrees.org.uk>
2017-02-03 15:56:23 +0000

    Remove old 'assets' table

--- db/schema.rb
+++ db/schema.rb
@@ -12,12 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20170112140915) do
 
-  create_table "assets", force: :cascade do |t|
-    t.string   "filename"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "items", force: :cascade do |t|
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
