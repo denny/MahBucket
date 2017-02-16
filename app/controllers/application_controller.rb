1c33d828730cec91de4947d1cb016b998a99561d
Denny de la Haye <denny.d@38degrees.org.uk>
2017-02-16 12:34:40 +0000

    Add IP whitelist

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,25 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action do
+  before_action :check_ip_whitelist
+  before_action :authenticate
+
+  private
+
+  def check_ip_whitelist
+    if Rails.env.production?
+      if Rails.application.secrets.permitted_ips
+        unless Rails.application.secrets.permitted_ips.split(',').include? request.ip
+          render text: 'Access Denied', status: :unauthorized
+        end
+      end
+    end
+  end
+
+  def authenticate
     unless session[:email] or request.path =~ /google_oauth2/
       redirect_to '/auth/google_oauth2'
     end
   end
+
 end
