a557819b0d92295aff21727c5314ee611ce491fc
Denny de la Haye <denny.d@38degrees.org.uk>
2017-02-23 17:31:09 +0000

    Display files in 'most recently uploaded first' order on index page.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,7 @@ class ItemsController < ApplicationController
   # GET /items
   # GET /items.json
   def index
-    @items = Item.all
+    @items = Item.all.order( created_at: :desc )
   end
 
   # GET /items/1
