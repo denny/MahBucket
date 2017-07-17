1d3ba3d61d6e36798f07e6d2e3ccd78e05e8e920
James Rees <jarees@gmail.com>
2017-07-17 16:15:56 +0100

    work for non-images

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,12 @@
 class Item < ApplicationRecord
   # has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  has_attached_file :file, styles: { display: "800x800>", small: "400x400>", thumbnail: "100x100>" }
+  has_attached_file :file, styles: lambda { |attachment|
+    if attachment.instance.image?
+      {display: "800x800>", small: "400x400>", thumbnail: "100x100>" }
+    else
+      {}
+    end
+  }
   # validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
   do_not_validate_attachment_file_type :file
   # Tagging
@@ -16,3 +22,4 @@ class Item < ApplicationRecord
     file_content_type =~ %r{^(image|(x-)?application)/(bmp|gif|jpeg|jpg|pjpeg|png|x-png)$}
   end
 end
+
