ea57cac8883cc8cc5828652bbaf4bf20f67ef763
Denny de la Haye <denny@38degrees.org.uk>
2020-05-23 05:02:01 +0100

    Add non-image file to test data

--- spec/features/user_views_items_spec.rb
+++ spec/features/user_views_items_spec.rb
@@ -3,19 +3,23 @@ require 'rails_helper'
 RSpec.feature "User views items", type: :feature, js: true do
     scenario 'successfully' do
         visit '/'
+
         expect( page ).to have_css 'a[href="/search"]'
     end
-    
+
     scenario 'sees "no items yet" notice' do
         visit '/'
+
         expect( page ).to have_text 'You have not added any items yet'
     end
-    
-    scenario 'sees newly-added item' do
-        Item.create!(
-            :file => File.new( Rails.root + 'spec/fixtures/mah-bucket.jpg' )
-        )
-        visit '/'
-        expect( page ).to have_link 'mah-bucket.jpg'
+
+    scenario 'sees newly-added items' do
+      Item.create!( file: File.new( Rails.root + 'spec/fixtures/mah-bucket.jpg' ) )
+      Item.create!( file: File.new( Rails.root + 'spec/fixtures/test.txt' ) )
+
+      visit '/'
+
+      expect( page ).to have_link 'mah-bucket.jpg'
+      expect( page ).to have_link 'test.txt'
     end
 end
