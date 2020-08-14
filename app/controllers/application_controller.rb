a02ebd5f101f5956d1ce0a004017106fcc4fd442
Denny de la Haye <2020@denny.me>
2020-08-14 16:49:35 +0100

    WIP on adding comment-able IP lists in ENV

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,17 +2,34 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :check_ip_whitelist
+  before_action :check_permitted_ips
   before_action :authenticate
 
   private
 
-  def check_ip_whitelist
+  def check_permitted_ips
     return if Rails.env.development?
     return if Rails.application.secrets.permitted_ips.blank?
-    return if Rails.application.secrets.permitted_ips.split( /[, ]/ ).include? request.ip
+
+    ip_addresses = list_of_permitted_ips( Rails.application.secrets.permitted_ips )
+    return if ip_addresses.include? request.ip
 
     render plain: 'Access Denied', status: :unauthorized
+  end
+
+  def list_of_permitted_ips( permitted_ips )
+    ip_addresses = []
+    permitted_ips_lines = permitted_ips.split( /\n/ )
+
+    if permitted_ips_lines.size > 1 || permitted_ips.include?('#')
+      permitted_ips_lines.each do |line|
+        ip_address = line.matches( /^([a-f\d.:]+)[#\s]?.*$/ )
+        ip_addresses << ip_address
+      end
+      return ip_addresses
+    end
+
+    permitted_ips.split( /[, ]/ )
   end
 
   def authenticate
