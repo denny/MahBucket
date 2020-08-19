c1402811c365f0f6f40b3b908ecfad0af12caa52
Denny de la Haye <2020@denny.me>
2020-08-20 00:06:49 +0100

    Add support for PERMITTED_IPS list to have comments (in brackets or or behind #)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   before_action :check_permitted_ips
   before_action :authenticate
 
-  def self.permitted_ips
+  def self.permitted_ips_from_env
     ENV['PERMITTED_IPS']
   end
 
@@ -13,27 +13,35 @@ class ApplicationController < ActionController::Base
 
   def check_permitted_ips
     return if Rails.env.development?
-    return if self.class.permitted_ips.blank?
+    return if self.class.permitted_ips_from_env.blank?
 
-    ip_addresses = list_of_permitted_ips( self.class.permitted_ips )
+    ip_addresses = list_of_permitted_ips( self.class.permitted_ips_from_env )
     return if ip_addresses.include? request.ip
 
     render plain: 'Access Denied', status: :unauthorized
   end
 
-  def list_of_permitted_ips( permitted_ips )
-    ip_addresses = []
+  def list_of_permitted_ips( ip_list_from_env )
+    # Remove anything inside brackets - e.g. '127.0.0.1 (localhost)' -> '127.0.0.1 '
+    permitted_ips = ip_list_from_env.gsub( /{\([^)]+\)/, '' )
+
+    # Handle the simple case (not multiline, no #comments embedded)
+    return permitted_ips.split( /[, ]+/ ) unless permitted_ips.match?( /[\n#]/ )
+
+    # Handle multi-line and/or comments
+    handle_ip_list_with_comments( permitted_ips )
+  end
+
+  def handle_ip_list_with_comments( permitted_ips )
     permitted_ips_lines = permitted_ips.split( /\n/ )
 
-    if permitted_ips_lines.size > 1 || permitted_ips.include?('#')
-      permitted_ips_lines.each do |line|
-        matches = line.match( /^(?<ip_address>[a-f\d.:]+)[#\s]?.*$/ )
-        ip_addresses << matches['ip_address'] if matches['ip_address'].present?
-      end
-      return ip_addresses
+    ip_addresses = []
+    permitted_ips_lines.each do |line|
+      matches = line.match( /^(?<ip_address>[a-f\d.:]+)[#\s]?.*$/ )
+      ip_addresses << matches['ip_address'] if matches['ip_address'].present?
     end
 
-    permitted_ips.split( /[, ]/ )
+    ip_addresses
   end
 
   def authenticate
